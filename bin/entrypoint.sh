0f7dc99b9b0cf15cbba73ba37005d8a0f428d654
Davide Doronzo <davide.doronzo@gmail.com>
2024-05-11 11:26:15 +0100

    feat(docker): add required timezone environment variable

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,13 +3,14 @@
 set -e
 set -u
 
+( : $TZ )
 ( : $NODE_ENV )
-( : $HSWS_PORT ) 
+( : $HSWS_PORT )
 ( : $HSWS_VERSION )
 ( : $HSWS_REVISION )
 ( : $HSWS_LOG_LEVEL )
-( : $STSA_SMART_APP_ID ) 
-( : $STSA_SMART_APP_CLIENT_ID ) 
-( : $STSA_SMART_APP_CLIENT_SECRET ) 
+( : $STSA_SMART_APP_ID )
+( : $STSA_SMART_APP_CLIENT_ID )
+( : $STSA_SMART_APP_CLIENT_SECRET )
 
 exec "$@"
