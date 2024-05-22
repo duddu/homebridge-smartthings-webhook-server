2c2f8ea92132c74c8ccbb5ee0742e7a235dd55ab
Davide Doronzo <davide.doronzo@gmail.com>
2024-05-23 00:38:38 +0100

    refactor(entrypoint): double quote environment variables

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,18 +3,18 @@
 set -e
 set -u
 
-( : $TZ )
-( : $NODE_ENV )
-( : $HSWS_PORT )
-( : $HSWS_VERSION )
-( : $HSWS_REVISION )
-( : $HSWS_LOG_LEVEL )
-( : $HSWS_REDIS_HOST )
-( : $HSWS_REDIS_PORT )
-( : $HSWS_REDIS_PASSWORD )
-( : $HSWS_REDIS_TLS_ENABLED )
-( : $STSA_SMART_APP_ID )
-( : $STSA_SMART_APP_CLIENT_ID )
-( : $STSA_SMART_APP_CLIENT_SECRET )
+( : "$TZ" )
+( : "$NODE_ENV" )
+( : "$HSWS_PORT" )
+( : "$HSWS_VERSION" )
+( : "$HSWS_REVISION" )
+( : "$HSWS_LOG_LEVEL" )
+( : "$HSWS_REDIS_HOST" )
+( : "$HSWS_REDIS_PORT" )
+( : "$HSWS_REDIS_PASSWORD" )
+( : "$HSWS_REDIS_TLS_ENABLED" )
+( : "$STSA_SMART_APP_ID" )
+( : "$STSA_SMART_APP_CLIENT_ID" )
+( : "$STSA_SMART_APP_CLIENT_SECRET" )
 
 exec "$@"
